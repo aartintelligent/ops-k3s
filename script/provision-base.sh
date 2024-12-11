71f46f758bcff164b084aecb512365903a6f8942
Aurelien Andre <aurelien.andre@aartintelligent.fr>
2024-12-11 19:17:27 +0100

    initial commit

--- script/provision-base.sh
+++ script/provision-base.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e
 
-install -m 0755 -d /etc/apt/keyring
+sudo install -m 0755 -d /etc/apt/keyring
 
-apt-get update && \
-apt-get upgrade -y && \
-apt-get install -y \
+sudo apt-get update && \
+sudo apt-get upgrade -y && \
+sudo apt-get install -y \
   ca-certificates \
   curl
 
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyring/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyring/docker.asc
 
-chmod a+r /etc/apt/keyring/docker.asc
+sudo chmod a+r /etc/apt/keyring/docker.asc
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyring/docker.asc] \
 https://download.docker.com/linux/debian \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
-| tee /etc/apt/sources.list.d/docker.list > /dev/null
+| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 USERS=(vagrant rootless)
 
@@ -29,13 +29,13 @@ for USER in "${USERS[@]}"; do
 
   if id "$USER" &>/dev/null; then
 
-    usermod -aG docker ${USER}
+    sudo usermod -aG docker ${USER}
 
   fi
 
 done
 
-systemctl reload docker
+sudo systemctl reload docker
 
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644
 
@@ -45,17 +45,17 @@ for USER in "${USERS[@]}"; do
 
   if id "$USER" &>/dev/null; then
 
-    mkdir -p /home/${USER}/.kube
+    sudo mkdir -p /home/${USER}/.kube
 
-    cp /etc/rancher/k3s/k3s.yaml /home/${USER}/.kube/config
+    sudo cp /etc/rancher/k3s/k3s.yaml /home/${USER}/.kube/config
 
-    chmod 644 /home/${USER}/.kube/config
+    sudo chmod 644 /home/${USER}/.kube/config
 
-    chown ${USER}:${USER} /home/${USER}/.kube/config
+    sudo chown ${USER}:${USER} /home/${USER}/.kube/config
 
-    chmod o-r /home/${USER}/.kube/config
+    sudo chmod o-r /home/${USER}/.kube/config
 
-    chmod g-r /home/${USER}/.kube/config
+    sudo chmod g-r /home/${USER}/.kube/config
 
   fi
 
